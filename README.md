016d76f6948815d669d05b455a6181edc154f92a
Shoyombo Gabriel Ayomide <74562680+DrPrime01@users.noreply.github.com>
2022-02-08 00:29:34 +0100

    Error corrections
    in the dom and some parts of the variables. I mistakenly assigned 'RBG' instead of 'RGB'

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Background-Changer
  A background changer using rgb
+ The project is inspired by a 100 JavaScript projects challenge I came across.
+ The first project was to create a background changer using loops to pick already written colors in a list. However, I decided to modify mine using Math.floor(Math.random * 265) to randomly pick a number between 0 and 255 and use it as an RGB value.
+
+ Feel free to add your suggestions.
--- script.js
+++ script.js
@@ -1,20 +1,41 @@
 const body = document.body;
-const button = document.querySelector('button');
-const textColor = document.getElementById("rbg");
+const button = document.querySelector("button");
+const textColor = document.getElementById("rgb");
 
-button.addEventListener('click', changeBG)
+button.addEventListener('click', changeBG);
 
 function changeRGB() {
-    return Math.floor(Math.random()*256);
+    return Math.floor(Math.random() * 256);
 }
 
 function changeBG() {
-    let color1 = changeRBG();
-    let color2 = changeRBG();
-    let color3 = changeRBG();
+    let color1 = changeRGB();
+    let color2 = changeRGB();
+    let color3 = changeRGB();
 
-    let rgb = `rgb(${color1}, ${color2}, ${color3})`
+    let rgb = `rgb(${color1}, ${color2}, ${color3})`;
     body.style.backgroundColor = rgb;
 
     textColor.innerText = rgb;
 }
+
+/*const body = document.body;
+const button = document.querySelector("button");
+const textColor = document.getElementById("rgb");
+
+button.addEventListener("click", changeBG);
+
+function changeBG() {
+  let color1 = changeRGB();
+  let color2 = changeRGB();
+  let color3 = changeRGB();
+
+  let rgb = `rgb(${color1}, ${color2}, ${color3})`;
+  body.style.backgroundColor = rgb;
+  console.log(rgb);
+  textColor.innerText = rgb;
+}
+
+function changeRGB() {
+  return Math.floor(Math.random() * 256);
+}*/
